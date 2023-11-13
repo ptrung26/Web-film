20614a8db6f88bc078fa91c6584c774ed5ea5af1
trungdev26 <80035418+ptrung26@users.noreply.github.com>
2023-11-13 11:10:42 +0700

    delete check user is exist to test the function

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -5,9 +5,6 @@ import responseHandler from "../handlers/responseHandler.js";
 const signup = async (req, res) => {
   try {
     const { username, password, color } = req.body;
-    const checkUser = await userModel.findOne({ username });
-    if (checkUser)
-      return responseHandler.badrequest(res, "username already used");
 
     const user = new userModel();
 
